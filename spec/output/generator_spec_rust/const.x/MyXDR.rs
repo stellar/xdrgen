19b52fc1368d5f571060bd8ec85cfbeb62a937c8
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-07-05 21:14:13 -0700

    Rust: Add additional conversion fns for array types

--- spec/output/generator_spec_rust/const.x/MyXDR.rs
+++ spec/output/generator_spec_rust/const.x/MyXDR.rs
@@ -8,7 +8,7 @@ pub const XDR_FILES_SHA256: [(&str, &str); 1] = [
   ("spec/fixtures/generator/const.x", "0bff3b37592fcc16cad2fe10b9a72f5d39d033a114917c24e86a9ebd9cda9c37")
 ];
 
-use core::{fmt, fmt::Debug, ops::Deref};
+use core::{array::TryFromSliceError, fmt, fmt::Debug, ops::Deref};
 
 #[cfg(feature = "std")]
 use core::marker::PhantomData;
@@ -81,6 +81,12 @@ impl fmt::Display for Error {
             #[cfg(feature = "std")]
             Error::Io(e) => write!(f, "{}", e),
         }
+    }
+}
+
+impl From<TryFromSliceError> for Error {
+    fn from(_: TryFromSliceError) -> Error {
+        Error::LengthMismatch
     }
 }
 
@@ -571,6 +577,15 @@ impl<T, const MAX: u32> AsRef<Vec<T>> for VecM<T, MAX> {
     #[must_use]
     fn as_ref(&self) -> &Vec<T> {
         &self.0
+    }
+}
+
+#[cfg(feature = "alloc")]
+impl<T: Clone, const MAX: u32> TryFrom<&Vec<T>> for VecM<T, MAX> {
+    type Error = Error;
+
+    fn try_from(v: &Vec<T>) -> Result<Self> {
+        v.as_slice().try_into()
     }
 }
 
