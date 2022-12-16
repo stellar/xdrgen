3d40bfbe07e8292b15d4e3b39b6b23659673d6ab
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-12-16 15:49:16 -0800

    fix

--- lib/xdrgen/generators/rust/src/types.rs
+++ lib/xdrgen/generators/rust/src/types.rs
@@ -1844,7 +1844,7 @@ mod tests {
         let res = VecM::<u8, 8>::read_xdr(&mut buf);
         match res {
             Err(Error::Io(_)) => (),
-            _ => panic!("expected IO error got {:?}", res),
+            _ => panic!("expected IO error got {res:?}"),
         }
     }
 
@@ -1854,7 +1854,7 @@ mod tests {
         let res = VecM::<u8, 8>::read_xdr(&mut buf);
         match res {
             Err(Error::NonZeroPadding) => (),
-            _ => panic!("expected NonZeroPadding got {:?}", res),
+            _ => panic!("expected NonZeroPadding got {res:?}"),
         }
     }
 
@@ -1894,7 +1894,7 @@ mod tests {
         let res = <[u8; 1]>::read_xdr(&mut buf);
         match res {
             Err(Error::Io(_)) => (),
-            _ => panic!("expected IO error got {:?}", res),
+            _ => panic!("expected IO error got {res:?}"),
         }
     }
 
@@ -1904,7 +1904,7 @@ mod tests {
         let res = <[u8; 1]>::read_xdr(&mut buf);
         match res {
             Err(Error::NonZeroPadding) => (),
-            _ => panic!("expected NonZeroPadding got {:?}", res),
+            _ => panic!("expected NonZeroPadding got {res:?}"),
         }
     }
 
