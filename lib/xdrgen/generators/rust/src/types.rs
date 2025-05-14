b795ebc2534da8c14972262405fb83a16f8eb3d2
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-05-14 23:31:45 +1000

    fix string deserialization of i64 from streaming json

--- lib/xdrgen/generators/rust/src/types.rs
+++ lib/xdrgen/generators/rust/src/types.rs
@@ -2325,10 +2325,12 @@ impl<'de> serde_with::DeserializeAs<'de, i64> for NumberOrString {
         #[derive(Deserialize)]
         #[serde(untagged)]
         enum I64OrString<'a> {
-            String(&'a str),
+            Str(&'a str),
+            String(String),
             I64(i64),
         }
         match I64OrString::deserialize(deserializer)? {
+            I64OrString::Str(s) => s.parse().map_err(serde::de::Error::custom),
             I64OrString::String(s) => s.parse().map_err(serde::de::Error::custom),
             I64OrString::I64(v) => Ok(v),
         }
