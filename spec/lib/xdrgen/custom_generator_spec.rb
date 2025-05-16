396c061d5c25dd0e73921871ce3f42a9745d4d26
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-05-16 22:28:50 +1000

    improve test

--- spec/lib/xdrgen/custom_generator_spec.rb
+++ spec/lib/xdrgen/custom_generator_spec.rb
@@ -1,22 +1,21 @@
 require 'spec_helper'
 
 describe Xdrgen::Generators do
-  generator_fixture_paths.each do |path|
-    it "can accept a custom generator to generate #{File.basename path}" do
-      expect(TestGenerator).to receive(:new).with(
-        an_instance_of(Xdrgen::AST::Top),
-        an_instance_of(Xdrgen::Output),
-        "namespace",
-        {},
-      ).and_call_original
-      expect_any_instance_of(TestGenerator).to receive(:generate).with(no_args)
-      Xdrgen::Compilation.new(
-        [path],
-        output_dir: "#{SPEC_ROOT}/output/custom_generator_spec/#{File.basename path}",
-        generator: TestGenerator,
-        namespace: "namespace",
-      ).compile
-    end
+  it "can accept a custom generator to generate" do
+    expect(TestGenerator).to receive(:new).with(
+      an_instance_of(Xdrgen::AST::Top),
+      an_instance_of(Xdrgen::Output),
+      "namespace",
+      { option: true },
+    ).and_call_original
+    expect_any_instance_of(TestGenerator).to receive(:generate).with(no_args)
+    Xdrgen::Compilation.new(
+      generator_fixture_paths,
+      output_dir: "output_dir/",
+      generator: TestGenerator,
+      namespace: "namespace",
+      options: { option: true }
+    ).compile
   end
 end
 
